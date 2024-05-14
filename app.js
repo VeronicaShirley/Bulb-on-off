ffc6fd2aa90a197f9acd756ad1a85a3731299477
VeronicaShirley <veronicamaryshirley20@gmail.com>
2024-05-14 20:41:43 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var bulb_on = document.getElementById("ON");
+var bulb_off = document.getElementById("OFF");
+var img = document.getElementById("bulb");
+
+function ON()
+{
+    img.src="https://storage.needpix.com/rsynced_images/light-148483_1280.png";
+}
+
+function OFF()
+{
+    img.src="https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_1280.png";
+}
